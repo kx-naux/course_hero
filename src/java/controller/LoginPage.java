9054f908cd6618e4878e867dc3e924cd86b930d1
YuBeng <wooyubeng2004@gmail.com>
2024-04-19 22:58:07 +0800

    Commit before changing the server

--- src/java/controller/LoginPage.java
+++ src/java/controller/LoginPage.java
@@ -1,9 +1,9 @@
 package controller;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 
---
+++ src/java/model/AccountDA.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import entity.Account;
+import java.sql.*;
+
+/**
+ *
+ * @author Woo Yu Beng
+ */
+
+public class AccountDA {
+    private String host = "jdbc:derby://localhost:1527/OnlineCourseAppDB";
+    private String user = "nbuser";
+    private String password = "nbuser";
+    private String tableName = "Accounts";
+    private Connection conn;
+    private PreparedStatement stmt;
+    
+    public AccountDA() throws SQLException{
+        createConnection();
+    }
+      
+    private void createConnection() throws SQLException{
+        conn = DriverManager.getConnection(host, user, password);
+    }
+    
+    public void shutDownConnection() throws SQLException{
+        if (conn != null)
+            conn.close();
+    }
+    
+    //Create Retrieve Update Delete
+    public void createRecord(Account account) throws SQLException{
+        String query = "INSERT INTO " + tableName + " VALUES(?,?,?,?)";
+        stmt = conn.prepareStatement(query);
+        
+        stmt.setString(1, account.getAccountID());
+        stmt.setString(2, account.getEmail());
+        stmt.setString(3, account.getSaltedPassword());
+        stmt.setString(4, account.getSalt());
+        
+        stmt.executeUpdate();
+    }
+    
+    public Account retrieveRecord(String accountID) throws SQLException{
+        Account account = null;
+        String query = "SELECT * FROM " + tableName + " WHERE ACCOUNT_ID = ?";
+        stmt = conn.prepareStatement(query);
+        
+        stmt.setString(1, accountID);
+        ResultSet rs = stmt.executeQuery();
+        
+        if(rs.next()){
+            account = new Account(rs.getString("ACCOUNT_ID"), rs.getString("EMAIL"), rs.getString("SALTEDPASSWORD"), rs.getString("SALT"));
+        }
+                
+        return account;
+    }
+    
+    public void updateRecord(Account account) throws SQLException{
+        
+    }
+    
+    public void deleteRecord(Account account) throws SQLException{
+        
+    }   
+}
