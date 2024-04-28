077245dad7201d89b2be461e01d5fd687533c643
Kah Xuan <jarretlow123@gmail.com>
2024-04-28 14:23:01 +0800

    Add Search Result Page

---
+++ src/java/controller/SearchResultPage.java
@@ -0,0 +1,21 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Search Result Page", urlPatterns = {"/search"})
+public class SearchResultPage extends HttpServlet {
+
+     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
+        
+        // Forward the request to home.jsp
+        request.getRequestDispatcher("/WEB-INF/Client/SearchResult.jsp").forward(request, response);
+    }
+    
+}
