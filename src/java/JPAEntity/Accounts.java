1871acc21e95432a9635b00b8b69aa6d4a9b26f1
User <User@Liang>
2024-04-29 01:51:13 +0800

    Initialize JBA and create JBAEntity for Accounts

---
+++ src/java/JPAEntity/Accounts.java
@@ -0,0 +1,129 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package JPAEntity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author User
+ */
+@Entity
+@Table(name = "ACCOUNTS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Accounts.findAll", query = "SELECT a FROM Accounts a"),
+    @NamedQuery(name = "Accounts.findByAccountId", query = "SELECT a FROM Accounts a WHERE a.accountId = :accountId"),
+    @NamedQuery(name = "Accounts.findByEmail", query = "SELECT a FROM Accounts a WHERE a.email = :email"),
+    @NamedQuery(name = "Accounts.findBySaltedpassword", query = "SELECT a FROM Accounts a WHERE a.saltedpassword = :saltedpassword"),
+    @NamedQuery(name = "Accounts.findBySalt", query = "SELECT a FROM Accounts a WHERE a.salt = :salt")})
+public class Accounts implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 9)
+    @Column(name = "ACCOUNT_ID")
+    private String accountId;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "EMAIL")
+    private String email;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 64)
+    @Column(name = "SALTEDPASSWORD")
+    private String saltedpassword;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "SALT")
+    private String salt;
+
+    public Accounts() {
+    }
+
+    public Accounts(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public Accounts(String accountId, String email, String saltedpassword, String salt) {
+        this.accountId = accountId;
+        this.email = email;
+        this.saltedpassword = saltedpassword;
+        this.salt = salt;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSaltedpassword() {
+        return saltedpassword;
+    }
+
+    public void setSaltedpassword(String saltedpassword) {
+        this.saltedpassword = saltedpassword;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (accountId != null ? accountId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Accounts)) {
+            return false;
+        }
+        Accounts other = (Accounts) object;
+        if ((this.accountId == null && other.accountId != null) || (this.accountId != null && !this.accountId.equals(other.accountId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "JPAEntity.Accounts[ accountId=" + accountId + " ]";
+    }
+    
+}
