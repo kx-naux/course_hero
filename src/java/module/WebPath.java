e6bbd811858dd487273c19233268edf1efc65ee0
Kah Xuan <jarretlow123@gmail.com>
2024-05-01 13:42:33 +0800

    Update Web Path

--- src/java/module/WebPath.java
+++ src/java/module/WebPath.java
@@ -9,34 +9,32 @@ public class WebPath {
 
     public WebPath() {
         pageUrls = new HashMap<>();
-        
+
         //client
         pageUrls.put("login", "/login");
-        pageUrls.put("sign up","/sign-up");
-        pageUrls.put("forget password","/forget-pw");
+        pageUrls.put("logout", "/Logout");
+        pageUrls.put("sign up", "/sign-up");
+        pageUrls.put("forget password", "/forget-pw");
         pageUrls.put("home", "/home");
         pageUrls.put("about us", "/about-us");
         pageUrls.put("products", "/courses");
         pageUrls.put("author", "/author");
         pageUrls.put("learning", "/learning");
-        pageUrls.put("cart","/cart");
-        pageUrls.put("wishlist","/wishlist");
+        pageUrls.put("cart", "/cart");
+        pageUrls.put("wishlist", "/wishlist");
         pageUrls.put("merchandises", "/merchandises");
         pageUrls.put("merchandise", "/merch");
-      
-        pageUrls.put("ErrorLogin","/ErrorLogin");
-      
-        
+
         //admin
-        pageUrls.put("dashboard","/admin/dashboard");
+        pageUrls.put("dashboard", "/admin/dashboard");
     }
 
     public String getPageUrl(String page) {
-    String url = pageUrls.get(page);
-    if (url != null) {
-        return "http://localhost:8080/course_hero" + url;
-    } else {
-        return "#";
+        String url = pageUrls.get(page);
+        if (url != null) {
+            return "http://localhost:8080/course_hero" + url;
+        } else {
+            return "#";
+        }
     }
-}
 }
