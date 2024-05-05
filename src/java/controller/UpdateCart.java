b92a4343c20e31641d047636f56aa4867754fd1e
Kah Xuan <jarretlow123@gmail.com>
2024-05-06 02:00:55 +0800

    Update add to cart

--- src/java/controller/UpdateCart.java
+++ src/java/controller/UpdateCart.java
@@ -8,6 +8,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
 import javax.annotation.Resource;
 import javax.persistence.EntityManager;
@@ -26,6 +27,7 @@ public class UpdateCart extends HttpServlet {
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // retreive from js submission
         // Read the request body
         BufferedReader reader = request.getReader();
         StringBuilder requestBody = new StringBuilder();
@@ -40,44 +42,66 @@ public class UpdateCart extends HttpServlet {
         
         // Get JSON data
         String productID = jsonObject.get("productID").getAsString();
-        System.out.println(productID);
+//        String userID = jsonObject.get("userID").getAsString();
+//        int quantity = jsonObject.get("quantity").getAsInt();
+//        String productType = jsonObject.get("productType").getAsString();
+//        String actionType = jsonObject.get("actionType").getAsString();
+//        Date timeAdded = new Date();
+//        
+//        // process the data
+//               
+//        //Getting Counters for Cart Item
+//        TablesRecordCounter currentItemTableCounter = em.find(TablesRecordCounter.class, "TablesRecordCounter");
+//        
+//        //Saving into the database
+//        Product currentProduct = em.find(Product.class, productID);
+//        Users currentUser = em.find(Users.class, userID);
+//        
+//        CartItems newCartItem = new CartItems();
+//        
+//        newCartItem.setCartitemId(currentItemTableCounter.getCounter() + 1);
+//        newCartItem.setProductId(currentProduct);
+//        newCartItem.setUserId(currentUser);
+//        newCartItem.setQuantity(quantity);
+//        newCartItem.setTimeAdded(timeAdded);
+//        
+//        currentItemTableCounter.counterIncrementByOne();
+//        
+//        //Creating JSON data
+//        JsonObject newJsonObject = new JsonObject();
+//        newJsonObject.addProperty("ProductID", currentProduct.getProductId());
+//        newJsonObject.addProperty("ProductCategory", productType);
+//        if("Add".equals(actionType)){
+//            newJsonObject.addProperty("ProductName", currentProduct.getProdName());
+//            newJsonObject.addProperty("ProductPrice", currentProduct.getPrice());
+//            newJsonObject.addProperty("Quantity", quantity);
+//            newJsonObject.addProperty("ProductCategory", currentProduct.getProdcatId().getCategoryName());
+//            newJsonObject.addProperty("ProductImage", "TemporaryVar");
+//        }
+//        
+//        saveDataToDatabases(request, response, newCartItem, currentItemTableCounter, actionType);
         
-        String userID = jsonObject.get("userID").getAsString();
-        int quantity = jsonObject.get("quantity").getAsInt();
-        String productType = jsonObject.get("productType").getAsString();
-        String actionType = jsonObject.get("actionType").getAsString();
-        Date timeAdded = new Date();
-               
-        //Getting Counters for Cart Item
-        TablesRecordCounter currentItemTableCounter = em.find(TablesRecordCounter.class, "TablesRecordCounter");
+        // response back to client
+        JsonObject responseJson = new JsonObject();
+        responseJson.addProperty("productId", productID); 
+        responseJson.addProperty("productName", "The Ultimate Excel Programming"); 
+        responseJson.addProperty("productCategory", "Microsoft Excel"); 
+        responseJson.addProperty("productCategory", "Microsoft Excel"); 
+        responseJson.addProperty("prodctImgPath", "./img/course/beginner_excel.jpg"); 
+        responseJson.addProperty("prodctQty", "10.00"); 
         
-        //Saving into the database
-        Product currentProduct = em.find(Product.class, productID);
-        Users currentUser = em.find(Users.class, userID);
-        
-        CartItems newCartItem = new CartItems();
-        
-        newCartItem.setCartitemId(currentItemTableCounter.getCounter() + 1);
-        newCartItem.setProductId(currentProduct);
-        newCartItem.setUserId(currentUser);
-        newCartItem.setQuantity(quantity);
-        newCartItem.setTimeAdded(timeAdded);
-        
-        currentItemTableCounter.counterIncrementByOne();
-        
-        //Creating JSON data
-        JsonObject newJsonObject = new JsonObject();
-        newJsonObject.addProperty("ProductID", currentProduct.getProductId());
-        newJsonObject.addProperty("ProductCategory", productType);
-        if("Add".equals(actionType)){
-            newJsonObject.addProperty("ProductName", currentProduct.getProdName());
-            newJsonObject.addProperty("ProductPrice", currentProduct.getPrice());
-            newJsonObject.addProperty("Quantity", quantity);
-            newJsonObject.addProperty("ProductCategory", currentProduct.getProdcatId().getCategoryName());
-            newJsonObject.addProperty("ProductImage", "TemporaryVar");
-        }
-        
-        saveDataToDatabases(request, response, newCartItem, currentItemTableCounter, actionType);
+        // Convert JSON object to string
+        String responseJsonString = responseJson.toString();
+
+        // Set content type and status code for the response
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        // Write JSON string to response
+        PrintWriter out = response.getWriter();
+        out.print(responseJsonString);
+        out.flush();
     }
     
     public void saveDataToDatabases(HttpServletRequest request, HttpServletResponse response, CartItems newCartItem, TablesRecordCounter currentItemTableCounter, String actionType) throws ServletException, IOException{
