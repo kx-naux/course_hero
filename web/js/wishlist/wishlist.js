9bdfe4a5f0a98df21bac6f63707486f15b19514f
Kah Xuan <jarretlow123@gmail.com>
2024-05-07 14:47:32 +0800

    Update wishlist page

--- web/js/wishlist/wishlist.js
+++ web/js/wishlist/wishlist.js
@@ -1,2 +1,32 @@
+const wishNumber = document.getElementById("wishNumber");
+const wishContent = document.querySelector("div.wishlist-div");
+const wishEmpty = document.querySelector("div.empty-div");
+const removeBtns = document.querySelectorAll("button.remove-btn");
+const moveBtns = document.querySelectorAll("button.move-btn");
 
+// show or hide the div on load
+document.addEventListener('DOMContentLoaded',()=>{
+    if (parseInt(wishNumber.innerText) < 1) {
+        wishContent.style.display = "none";
+        wishEmpty.style.display = "flex";
+    }
+    else {
+        wishContent.style.display = "flex";
+        wishEmpty.style.display = "none";
+    }
+});
+
+// event listener for remove from wish
+removeBtns.forEach(remove=>{
+    remove.addEventListener('click',(evt)=>{
+        
+    });
+});
+
+// event listener for move to cart
+moveBtns.forEach(move=>{
+    move.addEventListener('click',(evt)=>{
+        
+    });
+});
 
