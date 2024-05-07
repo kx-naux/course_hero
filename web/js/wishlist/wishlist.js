62065812ef7d931d3171c45e4b728daaf54608b4
Kah Xuan <jarretlow123@gmail.com>
2024-05-07 15:32:41 +0800

    Complete wishlist page

--- web/js/wishlist/wishlist.js
+++ web/js/wishlist/wishlist.js
@@ -141,3 +141,52 @@ moveBtns.forEach(move => {
     });
 });
 
+// add item into cart list
+function addCartItem(data) {
+    let cartlistDiv = document.getElementById("cartlistDiv");
+    let cartlistLink = document.getElementById("cartlistLink");
+    let cartlistPrice = document.getElementById("cartlistPrice");
+    let cartEmptyDiv = document.getElementById("cartlistEmpty");
+    let cartlistNumber = document.getElementById("cartlistNumber");
+    let cartlistItems = document.querySelectorAll("div#cartlistDiv div.course-item");
+
+    if (data.productType === "course") {
+        // Create a new cart item element
+        let newItem = document.createElement("div");
+        let newItemPrice = data.productPrice.toFixed(2);
+        newItem.classList.add("course-item", "flex-row");
+        newItem.setAttribute("productID", data.productID);
+        newItem.setAttribute("productType", data.productType);
+        newItem.innerHTML = `
+            <div class="course-item-img">
+                <img src="${data.productImgPath}" alt="" draggable="false" />
+            </div>
+            <div class="course-item-info flex-col">
+                <h1 class="course-title">${data.productName}</h1>
+                <p class="course-author">${data.productCategory}</p>
+                <p class="course-price">RM ${newItemPrice}</p>
+            </div>
+         `;
+
+        // insert item
+        cartlistDiv.appendChild(newItem);
+
+        // udpate cart number
+        cartlistNumber.innerText = parseInt(cartlistNumber.innerText) + 1;
+    } else if (data.productType === "merchandise") {
+
+        // check is merch exist in list
+
+    }
+
+    // check list is empty before adding
+    if (cartlistItems.length < 1) {
+        cartlistDiv.classList.add("active");
+        cartlistLink.classList.add("active");
+        cartlistNumber.classList.add("active");
+        cartEmptyDiv.classList.remove("active");
+    }
+
+    // update price
+    cartlistPrice.innerText = (parseFloat(cartlistPrice.innerText) + data.productPrice).toFixed(2);
+}
