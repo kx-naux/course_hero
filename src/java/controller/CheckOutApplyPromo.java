0114a695a6abe66a2815e325d60c6d40575b4711
User <User@Liang>
2024-05-10 04:09:04 +0800

    check out update

--- src/java/controller/CheckOutApplyPromo.java
+++ src/java/controller/CheckOutApplyPromo.java
@@ -4,6 +4,7 @@
  */
 package controller;
 
+import JPAEntity.BillingAddress;
 import JPAEntity.Promotions;
 import JPAEntity.Users;
 import java.io.IOException;
@@ -60,6 +61,98 @@ public class CheckOutApplyPromo extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
+        //store billing address option    
+        String selectedBillingAddress = request.getParameter("storedAddress");
+        if(selectedBillingAddress!=null){
+            session.setAttribute("selectedBillingInfo",selectedBillingAddress);
+        }
+        if(selectedBillingAddress!=null && selectedBillingAddress.equals("newAddress")){
+            //store new billing address
+            String addLine1 = request.getParameter("address1");
+            String addLine2 = request.getParameter("address2");
+            if(addLine2 == null){
+                addLine2 = "";
+            }
+            String city = request.getParameter("city");
+            String postalCode = request.getParameter("postalCode");
+            String state = request.getParameter("state");
+            String country = request.getParameter("country");
+            BillingAddress newBillData = new BillingAddress(addLine1,city,state,postalCode,country);
+            newBillData.setLine2(addLine2);
+            session.setAttribute("userInputBillAddressCheckout",newBillData);
+        }
+        
+        //get is checked to update billing address
+        if(request.getParameter("storingAddress")==null){
+            session.setAttribute("chooseToUpdateBillAddress", false);
+        }else{
+            session.setAttribute("chooseToUpdateBillAddress", true);
+        }
+        
+        //get selected payment method
+        String selectedPaymentMethod = request.getParameter("paymentMethod");
+        session.setAttribute("selectedPaymentMethod", selectedPaymentMethod);
+        //store selected payment details
+        if(selectedPaymentMethod.equals("card")){
+            String cardHolderName = request.getParameter("cardHolder");
+            if(cardHolderName == null){
+                cardHolderName = "";
+            }
+            session.setAttribute("cardHolderNameInput", cardHolderName);
+            
+            String cardNumber = request.getParameter("cardNo");
+            if(cardNumber == null){
+                cardNumber = "";
+            }
+            session.setAttribute("cardNumberInput", cardNumber);
+            
+            String expDate = request.getParameter("expDate");
+            if(expDate == null){
+                expDate = "";
+            }
+            session.setAttribute("expDateInput", expDate);
+            
+            String ccv = request.getParameter("ccv");
+            if(ccv == null){
+                ccv = "";
+            }
+            session.setAttribute("ccvInput", ccv);
+            
+        }else if(selectedPaymentMethod.equals("bank")){
+            String selectedBankId = request.getParameter("selectedBank");
+            if(selectedBankId==null){
+                selectedBankId = "";
+            }
+            session.setAttribute("selectedOnlineBankId", selectedBankId);
+            
+            String selectedBankRemark = request.getParameter("bankRemark");
+            if(selectedBankRemark == null){
+                selectedBankRemark = "";
+            }
+            session.setAttribute("selectedOnlineBankRemark", selectedBankRemark);
+            
+        }else if(selectedPaymentMethod.equals("tng")){
+            String tngNo = request.getParameter("tngPhoneNo");
+            if(tngNo==null){
+                tngNo = "";
+            }
+            session.setAttribute("tngNoInputUser", tngNo);
+            
+            String tngRemark = request.getParameter("tngRemark");
+            if(tngRemark == null){
+                tngRemark = "";
+            }
+            session.setAttribute("tngRemarkInput", tngRemark);
+        }
+        
+        //get is check to update
+        if(request.getParameter("storingPayment")==null){
+            session.setAttribute("chooseToUpdateStoredPayment", false);
+        }else{
+            session.setAttribute("chooseToUpdateStoredPayment", true);
+        }
+        
+        
         session.removeAttribute("errMsg");
         session.removeAttribute("successMsg");
         
